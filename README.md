e91b4e72efd6b258b92c4f3a49914aa73add60f0
vanceinc <david.vance@centurylink.com>
2019-01-28 17:29:46 -0500

    your commit message

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Learning to track my code
 # mycode (Project Title)
 
-One Paragraph of your project description goes here. Describe what you're trying to do. What is the purpose of putting up this repo?
+learn how to version control projects with git.
 
 ## Getting Started
 
---
+++ mix-list/mix_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+my_list = [ "192.168.0.5", 5060, "UP" ]
+
+##printing the items inteh list
+
+print("The first item in the list (IP): " + my_list[0] )
+
+## using the string function using str
+
+print("The second item in the list (port): " + str(my_list[1]) )
+
+print("The last item in the list (state): " + my_list[2] )
+
+new_list = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+
+
